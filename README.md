ab692d8f10740d6e58a1d9671ba5fee3e06ca2f1
Gyunny <wjdrbs966@naver.com>
2021-01-30 23:01:32 +0900

    brute force adD

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # `Gyuuny's Algorithm Study`
 
 - ### `Brute force`
-    - [N과 M(1)](https://www.acmicpc.net/problem/15649)
+    - [N과 M(1)]()
 
 - ### `2021 KAKAO BLIND RECRUITMENT`
-    - [신규 아이디 추천]()
+    - [신규 아이디 추천](https://github.com/hoecholi-CS/Algorithm/blob/master/src/KAKAO_2021/New_Id.java)
     
     
--- src/Brute_Force/NM1.java
+++ src/Brute_Force/NM1.java
@@ -1,7 +1,41 @@
 package Brute_Force;
 
-public class NM1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class NM1 {
+    static int N;
+    static int M;
+    static boolean[] visit;
+    static int[] list;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+        M = input.nextInt();
+
+        visit = new boolean[N + 1];
+        list = new int[M + 1];
+        dfs(1);
+    }
+
+    static void dfs(int k) {
+        if (k == M + 1) {
+            for (int p = 1; p < list.length; ++p) {
+                System.out.print(list[p] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; ++i) {
+            if (!visit[i]) {
+                visit[i] = true;
+                list[k] = i;
+                dfs(k + 1);
+                visit[i] = false;
+            }
+        }
     }
 }
+
